1b56132e14d14e6ffe399e85e106c361f4c00719
Luiz <qluizmachado@gmail.com>
2026-02-15 02:16:37 -0300

    Initial commit

---
+++ src/main/kotlin/com/mach/test/Main.kt
@@ -0,0 +1,15 @@
+package com.mach.test
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        println("Wow, it worked!")
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
